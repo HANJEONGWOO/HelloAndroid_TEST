0cb6b9596a6a60e92c9de0c8a41ea6bec91fcf1b
Hanjeongwoo <hotgkswjddn@naver.com>
2021-02-28 21:25:01 +0900

    first commit

---
+++ app/src/main/java/org/techtown/helloandroid_jw/MainActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.helloandroid_jw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
